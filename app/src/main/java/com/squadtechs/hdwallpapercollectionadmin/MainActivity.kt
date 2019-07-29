d8947d8ff1167e0fb0ae13c150681c8733c62365
markhor <mohammadfaizan980@gmail.com>
2019-07-29 20:19:37 +0500

    Added Main UI and also adjusted theme and colors a bit

--- app/src/main/java/com/squadtechs/hdwallpapercollectionadmin/MainActivity.kt
+++ app/src/main/java/com/squadtechs/hdwallpapercollectionadmin/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.squadtechs.hdwallpapercollectionadmin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+        prepareToolbar()
     }
+
+    private fun prepareToolbar() {
+        toolbar.title = "Wallpapers"
+    }
+
+    private fun initViews() {
+        toolbar = findViewById(R.id.toolbar)
+    }
+
 }

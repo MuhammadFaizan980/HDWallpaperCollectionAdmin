b687197d0d615dba2d6959f0fbdc835807539d99
markhor <mohammadfaizan980@gmail.com>
2019-07-30 14:44:18 +0500

    inflated menu into Categories activity

--- app/src/main/java/com/squadtechs/hdwallpapercollectionadmin/activity_main/MainActivity.kt
+++ app/src/main/java/com/squadtechs/hdwallpapercollectionadmin/activity_main/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.squadtechs.hdwallpapercollectionadmin.activity_main
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.squadtechs.hdwallpapercollectionadmin.R
+import com.squadtechs.hdwallpapercollectionadmin.activity_add_category.ActivityAddCategory
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +21,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun prepareToolbar() {
         toolbar.title = "Wallpapers"
+        toolbar.inflateMenu(R.menu.menu_add)
+        toolbar.setOnMenuItemClickListener { item: MenuItem? ->
+            when (item!!.itemId) {
+                R.id.item_add -> {
+                    startActivity(Intent(this, ActivityAddCategory::class.java))
+                }
+            }
+            return@setOnMenuItemClickListener true
+        }
     }
 
     private fun initViews() {

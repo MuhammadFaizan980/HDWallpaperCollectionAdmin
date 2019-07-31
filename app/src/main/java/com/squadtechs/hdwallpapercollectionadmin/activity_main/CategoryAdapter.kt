ad1c7612e2f0bb9069af4a7cacaedad3f2c2134e
markhor <mohammadfaizan980@gmail.com>
2019-07-31 17:38:44 +0500

    Added model and adapter for categories list

--- app/src/main/java/com/squadtechs/hdwallpapercollectionadmin/activity_main/CategoryAdapter.kt
+++ app/src/main/java/com/squadtechs/hdwallpapercollectionadmin/activity_main/CategoryAdapter.kt
@@ -1,4 +1,57 @@
 package com.squadtechs.hdwallpapercollectionadmin.activity_main
 
-class CategoryAdapter {
+import android.app.Activity
+import android.content.Context
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squadtechs.hdwallpapercollectionadmin.R
+import com.squareup.picasso.Picasso
+
+
+class CategoryAdapter(val context: Context, val list: ArrayList<CategoryModel>) :
+    RecyclerView.Adapter<CategoryAdapter.CategoryHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryHolder {
+        return CategoryHolder(LayoutInflater.from(context).inflate(R.layout.cell_design, parent, false))
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun getItemViewType(position: Int): Int = position
+
+    override fun onBindViewHolder(holder: CategoryHolder, position: Int) {
+        adjustLayout(holder, position)
+        inflateValues(holder, position)
+    }
+
+    private fun inflateValues(holder: CategoryHolder, position: Int) {
+        holder.txtTitle.text = list[position].category_name
+        Picasso.get().load(list[position].category_image_url).into(holder.imgCell)
+    }
+
+    private fun adjustLayout(holder: CategoryHolder, position: Int) {
+        val displayMetrics = DisplayMetrics()
+        (context as Activity).windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val height = displayMetrics.heightPixels
+        val width = displayMetrics.widthPixels
+        holder.frame.layoutParams = FrameLayout.LayoutParams((width / 2), ((40 * height) / 100))
+        if (position % 2 == 0) {
+            holder.frame.setPadding(1, 1, 0, 0)
+        } else {
+            holder.frame.setPadding(0, 1, 1, 0)
+        }
+    }
+
+    inner class CategoryHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val txtTitle: TextView = view.findViewById(R.id.txt_title)
+        val frame: FrameLayout = view.findViewById(R.id.main_frame)
+        val touchView: View = view.findViewById(R.id.touch_view)
+        val imgCell: ImageView = view.findViewById(R.id.img_cell)
+    }
 }
--- app/src/main/java/com/squadtechs/hdwallpapercollectionadmin/activity_main/CategoryModel.kt
+++ app/src/main/java/com/squadtechs/hdwallpapercollectionadmin/activity_main/CategoryModel.kt
@@ -1,4 +1,6 @@
 package com.squadtechs.hdwallpapercollectionadmin.activity_main
 
 class CategoryModel {
+    public var category_image_url: String = ""
+    public var category_name: String = ""
 }
